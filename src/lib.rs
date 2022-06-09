30901a1cf57482a2e0bb9a9e20e09c146ba714b5
Johannes Kliemann <jk@jkliemann.de>
2022-06-09 23:46:25 +0200

    Import gpr2 interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+use std::{os::raw::{c_int, c_char}, ffi::CStr, str};
+
+extern "C" {
+    fn gpr2cinit();
+    fn gpr2cfinal();
+    fn gpr2_request(fun: c_int, request: *const c_char, answer: *mut *mut c_char) -> c_int;
+    fn gpr2_free_answer(answer: *const c_char);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
