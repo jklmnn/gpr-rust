3d1590c11770d656359f60bf6fa68da216971da3
Johannes Kliemann <jk@jkliemann.de>
2022-09-22 17:45:27 +0200

    Remove unneeded initialization and finalization

--- examples/ada_hello/src/main.rs
+++ examples/ada_hello/src/main.rs
@@ -1,14 +1,10 @@
 extern "C" {
-    fn adahelloinit();
-    fn adahellofinal();
     fn ada_hello();
 }
 
 fn main() {
     println!("Hello from Rust!");
     unsafe {
-        adahelloinit();
         ada_hello();
-        adahellofinal();
     }
 }
