fba8433a75fd7bb56b5704927d85ef31c1228474
aissata <aimaiga2@gmail.com>
2023-01-24 07:38:36 +0000

    reverted example from addition back to hello world

--- examples/ada_hello/ada_hello/src/ada_hello.adb
+++ examples/ada_hello/ada_hello/src/ada_hello.adb
@@ -2,10 +2,12 @@ with Ada.Text_IO;
 
 package body Ada_Hello is
 
-   procedure Hello
-   is
+   function Hello return Integer is
+      A : Integer := 7;
+      B : Integer := 9;
    begin
-      Ada.Text_IO.Put_Line ("Hello from Ada!");
+      -- Ada.Text_IO.Put_Line (Integer'Image(A+B));
+      return (A + B);
    end Hello;
 
 end Ada_Hello;
