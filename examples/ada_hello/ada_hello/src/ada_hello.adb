986493a6535bab155d04f7c74accb8e1d55a4539
aissata <aimaiga2@gmail.com>
2023-01-24 08:07:19 +0000

    Removed format on save + removed white spaces

--- examples/ada_hello/ada_hello/src/ada_hello.adb
+++ examples/ada_hello/ada_hello/src/ada_hello.adb
@@ -1,10 +1,9 @@
 with Ada.Text_IO;
 
-with Ada.Text_IO;
-
 package body Ada_Hello is
 
-   procedure Hello is
+   procedure Hello
+   is
    begin
       Ada.Text_IO.Put_Line ("Hello from Ada!");
    end Hello;
--- examples/ada_hello/ada_hello/src/ada_hello.ads
+++ examples/ada_hello/ada_hello/src/ada_hello.ads
@@ -1,6 +1,9 @@
-package Ada_Hello is
+package Ada_Hello
+is
 
-  procedure Hello with
-   Export, Convention => C, External_Name => "ada_hello";
+   procedure Hello with
+      Export,
+      Convention => C,
+      External_Name => "ada_hello";
 
 end Ada_Hello;
