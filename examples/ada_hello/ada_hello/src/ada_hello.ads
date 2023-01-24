9f4dd0f3cdbb094c0d8a2b48c1affc2d4e5da352
aissata <aimaiga2@gmail.com>
2023-01-24 07:38:36 +0000

    reverted example from addition back to hello world

--- examples/ada_hello/ada_hello/src/ada_hello.ads
+++ examples/ada_hello/ada_hello/src/ada_hello.ads
@@ -1,9 +1,6 @@
-package Ada_Hello
-is
+package Ada_Hello is
 
-   procedure Hello with
-      Export,
-      Convention => C,
-      External_Name => "ada_hello";
+   function Hello return Integer with
+     Export, Convention => C, External_Name => "ada_hello";
 
 end Ada_Hello;
